4d6de61cb3385facf6763a38555955e47cf339e8
Elena Kochegarova <lelet@ngs.ru>
2021-12-15 16:51:53 +0700

    Changed function solveEquation(a,b,c) in task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
+  let x1, x2;
+  let D = b**2 - 4*a*c;
+  
+  if (D < 0) {
+    arr = [];
+  } else if (D === 0) {
+    x1 = -b/(2*a);
+    arr = [x1];
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D))/(2*a); 
+    x2 = (-b - Math.sqrt(D))/(2*a);
+    arr = [x1, x2];
+  }
+
   // код для задачи №1 писать здесь
   return arr; // array
 }
