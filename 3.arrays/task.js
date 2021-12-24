7974b08c73164f9f0abe7a26e3cbb9f5534eebaa
Elena Kochegarova <lelet@ngs.ru>
2021-12-24 09:20:51 +0700

    Task 3.arrays done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.every((item, idx) => arr1.length === arr2.length && item === arr2[idx]);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number*10);
+  return resultArr;
 }
