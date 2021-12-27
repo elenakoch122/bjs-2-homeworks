d12f27d091ede8fd2acec87543f35ebc5276b34a
Elena Kochegarova <lelet@ngs.ru>
2021-12-27 11:15:41 +0700

    Task 4.objects done. Corrections on comments in Task 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result = arr1.every((item, idx) => arr1.length === arr2.length && item === arr2[idx]);
-  return result;
+  return result = arr1.every((item, idx) => arr1.length === arr2.length && item === arr2[idx]);
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number*10);
-  return resultArr;
+  return resultArr = arr.filter(number => number > 0 && number % 3 === 0).map(number => number*10);
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,56 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+      this.name = name,
+      this.gender = gender,
+      this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [...marks]; 
+    } else {
+      this.marks.push(...marks);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (const mark of this.marks) {
+    sum += mark;
+  }
+  return avg = sum/this.marks.length;
+}
+  
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student1.getAverage()); //4.6666
+console.log(student1);
+
+let student2 = new Student("Buzz", "female", 35);
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades')
+console.log(student2);
