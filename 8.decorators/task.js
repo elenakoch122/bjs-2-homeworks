467a8ebe4540cb74f4882afe84cc9fb264084427
Elena Kochegarova <lelet@ngs.ru>
2022-01-15 17:20:03 +0700

    Task_8.decorators done.

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,58 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  let amountCalls = 0;
+  return function (...rest) {
+    let key = rest.toString();
+    let idx = cache.findIndex(item => item.args === key);
+    if (idx === -1) {
+      amountCalls++;
+      if (amountCalls === 6) {
+        cache.shift();
+        amountCalls = 5;
+      }
+      let res = func(...rest);
+      cache.push({args: key, result: res});
+      console.log(`Вычисляем: ${res}`);
+      return `Вычисляем: ${res}`;
+    } else {
+      console.log(`Из кэша: ${cache[idx].result}`);
+      return `Из кэша: ${cache[idx].result}`;
+    }
+  }
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false;
+  let timeoutID;
+  return function (...args) {
+    if (isThrottled) {
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {isThrottled = false;}, ms);
+      return;
+    }
+    func.apply(this, ...args);
+    isThrottled = true;
+    timeoutID = setTimeout(() => {isThrottled = false;}, ms);
+    console.log(`timeoutID = ${timeoutID}`);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let isThrottled = false;
+  let timeoutID;
+  function wrapper (...args) {
+    if (isThrottled) {
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {isThrottled = false;}, ms);
+      wrapper.count++;
+      return;
+    }
+    func.apply(this, ...args);
+    isThrottled = true;
+    timeoutID = setTimeout(() => {isThrottled = false;}, ms);
+    wrapper.count++;
+    console.log(`timeoutID = ${timeoutID}`);
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
